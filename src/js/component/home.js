974fc5b85acd4e4acec3259333eca7d4164e0f1b
apolopino <apolo.pino@gmail.com>
2021-06-12 00:22:13 +0000

    code polish?

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -44,6 +44,9 @@ export function Home() {
 					<div className="col-md-3">
 						<Card />
 					</div>
+					<div className="col-md-3">
+						<Card />
+					</div>
 				</div>
 			</div>
 			<Footer copyright="Apolo Pino" />
